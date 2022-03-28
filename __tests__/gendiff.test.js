3de0c01f756a2c0c9b3552914b3496893803c052
Alexey Popov <popov76@gmail.com>
2022-03-28 22:00:53 +0300

    add stylish

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,5 +1,6 @@
 import { fileURLToPath } from 'url';
 import path from 'path';
+import { readFileSync } from 'fs';
 import compareFiles from '../src/compare.js';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -7,25 +8,27 @@ const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '__fixtures__', filename);
 
-const correctResult = `{
- - follow: false
-   host: hexlet.io
- - proxy: 123.234.53.22
- - timeout: 50
- + timeout: 20
- + verbose: true
-}`;
+const correctResult1 = readFileSync(getFixturePath('result1.txt'), 'utf-8');
+
+const correctResult2 = readFileSync(getFixturePath('result2.txt'), 'utf-8');
 
 test('compare json', () => {
   expect(compareFiles(
     getFixturePath('file1.json'),
     getFixturePath('file2.json'),
-  )).toBe(correctResult);
+  )).toBe(correctResult1);
 });
 
 test('compare yaml', () => {
   expect(compareFiles(
     getFixturePath('file1.yaml'),
     getFixturePath('file2.yaml'),
-  )).toBe(correctResult);
+  )).toBe(correctResult1);
+});
+
+test('compare json recurse', () => {
+  expect(compareFiles(
+    getFixturePath('file3.json'),
+    getFixturePath('file4.json'),
+  )).toBe(correctResult2);
 });
--- src/compare.js
+++ src/compare.js
@@ -3,6 +3,9 @@ import _ from 'lodash';
 import path from 'path';
 import yaml from 'js-yaml';
 
+const indentSymbol = ' ';
+const indentCount = 4;
+
 const getObjectFromFile = (filePath) => {
   let fileName = filePath;
   if (!filePath.startsWith('/')) {
@@ -24,38 +27,83 @@ const getObjectFromFile = (filePath) => {
   return parse(fileContent);
 };
 
-const getPropertyCompareString = (property, obj1, obj2) => {
-  const hasFirstObject = _.has(obj1, property);
-  const hasSecondObject = _.has(obj2, property);
-
-  let result;
-  if (hasFirstObject && !hasSecondObject) {
-    result = ` - ${property}: ${obj1[property]}`;
-  } else if (!hasFirstObject && hasSecondObject) {
-    result = ` + ${property}: ${obj2[property]}`;
-  } else if (hasFirstObject && hasSecondObject) {
-    if (obj1[property] === obj2[property]) {
-      result = `   ${property}: ${obj1[property]}`;
-    } else {
-      result = ` - ${property}: ${obj1[property]}\n + ${property}: ${obj2[property]}`;
+const compareObjects = (obj1, obj2) => {
+  const result = {};
+  const uniqKeys = _.union(_.keys(obj1), _.keys(obj2));
+  const sortedKeys = _.sortBy(uniqKeys);
+  sortedKeys.forEach((key) => {
+    if (_.has(obj1, key) && !_.has(obj2, key)) {
+      _.set(result, key, { has: 'firstOnly', value1: _.get(obj1, key) });
     }
-  }
+    if (!_.has(obj1, key) && _.has(obj2, key)) {
+      _.set(result, key, { has: 'secondOnly', value2: _.get(obj2, key) });
+    }
+    if (_.has(obj1, key) && _.has(obj2, key)) {
+      const value1 = _.get(obj1, key);
+      const value2 = _.get(obj2, key);
+      if (_.isObject(value1) && _.isObject(value2)) {
+        _.set(result, key, { has: 'bothObjects', value: compareObjects(value1, value2) });
+      } else if (value1 === value2) {
+        _.set(result, key, { has: 'bothEqual', value: value1 });
+      } else {
+        _.set(result, key, { has: 'bothNotEqual', value1, value2 });
+      }
+    }
+  });
   return result;
 };
 
-const compareObjects = (obj1, obj2) => {
-  const unitedKeys = [...Object.keys(obj1), ...Object.keys(obj2)];
-  const uniqKeys = _.uniq(unitedKeys);
-  const sortedKeys = _.sortBy(uniqKeys);
-  const result = sortedKeys.reduce((acc, property) => `${acc}\n${getPropertyCompareString(property, obj1, obj2)}`, '{');
-  return `${result}\n}`;
+const stylish = (object) => {
+  const indentString = (indent) => indentSymbol.repeat(indentCount * indent);
+
+  const addValue = (value, indent = 0) => {
+    if (_.isObject(value)) {
+      let res = '{';
+      _.forOwn(value, (val, key) => {
+        res = `${res}\n${indentString(indent + 1)}${key}: ${addValue(val, indent + 1)}`;
+      });
+      return `${res}\n${indentString(indent)}}`;
+    }
+    return value;
+  };
+
+  const addKey = (name, value, indent, prefix) => `\n${indentString(indent)}  ${prefix} ${name}: ${addValue(value, indent + 1)}`;
+
+  const formatObject = (obj, indent = 0) => {
+    let result = '{';
+    Object.keys(obj).forEach((key) => {
+      const keyDescription = _.get(obj, key);
+      switch (keyDescription.has) {
+        case 'firstOnly':
+          result += addKey(key, keyDescription.value1, indent, '-');
+          break;
+        case 'secondOnly':
+          result += addKey(key, keyDescription.value2, indent, '+');
+          break;
+        case 'bothEqual':
+          result += addKey(key, keyDescription.value, indent, ' ');
+          break;
+        case 'bothNotEqual':
+          result += addKey(key, keyDescription.value1, indent, '-');
+          result += addKey(key, keyDescription.value2, indent, '+');
+          break;
+        case 'bothObjects':
+          result = `${result}\n${indentString(indent + 1)}${key}: ${formatObject(keyDescription.value, indent + 1)}`;
+          break;
+        default:
+          throw new Error(`unknoun description: ${keyDescription.has}`);
+      }
+    });
+    return `${result}\n${indentString(indent)}}`;
+  };
+  return formatObject(object);
 };
 
 const compareFiles = (file1, file2) => {
   const obj1 = getObjectFromFile(file1);
   const obj2 = getObjectFromFile(file2);
-  const result = compareObjects(obj1, obj2);
-  return result;
+  const compareResult = compareObjects(obj1, obj2);
+  return stylish(compareResult);
 };
 
 export default compareFiles;
